ae6973fd607c8627d82e1742ec08bd350792d7d2
S-Tanabe <tanabe.shouji0330@gmail.com>
2017-12-24 22:33:59 +0900

    implement problems

--- index.php
+++ index.php
@@ -2,15 +2,20 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\App();
+$app->response->headers->set('Content-Type', 'application/json');
+
+$db = new PDO('sqlite:security.db');
+$db->exec('CREATE TABLE IF NOT EXISTS problems(id INTEGER PRIMARY KEY, contents TEXT)');
 
 $app->get('/', function(Slim\Http\Request $req, Slim\Http\Response $res, $args = [])
 {
     return $res->withStatus(400)->write('Bad Request');
 });
 
-$app->get('/list', function(Slim\Http\Request $req, Slim\Http\Response $res, $args = [])
+$app->get('/problems', function(Slim\Http\Request $req, Slim\Http\Response $res, $args = [])
 {
-    return $res->withStatus(200)->write('hogehoge');
+    $problems = $db->query('SELECT id, contents FROM problems ORDER BY ID DESC')->fetchAll(PDO::FETCH_ASSOC);
+    return $res->withStatus(200)->write(json_encode($problems));
 });
 
 $app->run();
